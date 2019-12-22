ca63291713453f22bbf8704350c59d51e2933871
MatusSeidl <seidl.matus@gmail.com>
2019-12-22 09:09:08 +0100

    add expaned and flex

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+      ],
+    );
   }
 }
