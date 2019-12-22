ac540213840213b9bb851eed746ff2d8cb0e0425
MatusSeidl <seidl.matus@gmail.com>
2019-12-22 09:35:35 +0100

    add flutbutton, and onPress

--- lib/main.dart
+++ lib/main.dart
@@ -18,17 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          flex: 1,
-          child: Image.asset('images/dice1.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print("Click Left");
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print("Click Right");
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
